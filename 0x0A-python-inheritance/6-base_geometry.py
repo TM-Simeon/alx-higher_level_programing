99eafdcab4681cbf88c924d8ba0bb070c9ef421d
TM-simeon <mnaan.simeon@st.futminna.edu.ng>
2022-08-02 00:32:11 +0100

    update

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a module to write an empty class"""
+
+
+class BaseGeometry:
+    """an empty class"""
+    def area(self):
+        raise Exception("area() is not implemented")
