5ee713ba956b973881db14653fe9832eab49ba9c
TM-simeon <mnaan.simeon@st.futminna.edu.ng>
2022-08-02 00:24:06 +0100

    update

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,8 +4,7 @@
 
 def inherits_from(obj, a_class):
     """check if it inherited"""
-    if not isinstance(obj, a_class):
-        if issubclass(type(obj), a_class):
-            return True
-    else:
+    if type(obj) == a_class:
         return False
+    if issubclass(type(obj), a_class):
+        return True
