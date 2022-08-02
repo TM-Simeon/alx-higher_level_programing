0bb31369bf8d4c7c304aea2495bb5bd2c38c620f
TM-simeon <mnaan.simeon@st.futminna.edu.ng>
2022-08-02 09:55:44 +0100

    update

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a module to write an empty class"""
+
+
+class BaseGeometry:
+    """an empty class"""
+    def __init__(self, width, height):
+        """the init method for the class"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """area method for the class"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """integer validator method for the class"""
+        self.name = name
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        self.value = value
