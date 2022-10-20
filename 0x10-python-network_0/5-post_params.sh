74c4dbf42516fa8b570c92ea26eedeafca339155
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-10-21 00:19:52 +0100

    update

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that sends a POST request with variables
-curl -s -X POST $1 -d "email:test@gmail.com" -d "subject:I will always be here for PLD"
+curl -s -X POST $1 -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
